46b3a85328fef28c80bd36c3398a6a3ae61cc214
Gabriel Gonzalez <ggonz296@stu.southtexascollege.edu>
2025-02-19 16:08:41 -0600

    Commiting useAuth

---
+++ src/composables/useAuth.js
@@ -0,0 +1,22 @@
+import router from '../router'
+import { firebaseApp } from './useFirebase'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
+
+const auth = getAuth(firebaseApp) //gets authentication from firebase
+
+const { isAuthenticated, user } = firbaseAuth(auth) //will validate user with firebase
+
+export const useAuth = () => {
+    const login = async (username, password) => {
+        await signInWithEmailAndPassword(auth, username, password)
+        return isAuthenticated.value
+    }
+
+    const logout = async () => {
+        await signOut(auth)
+        router.push({name: 'Home'})
+    }
+
+    return{isAuthenticated, user, login, logout}
+}
